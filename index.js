3d49720a5426172bb36380c1fbe0a64676cdd513
Siddhesh <sidd.mhapankar7@gmail.com>
2023-12-03 17:01:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+
 import express from "express";
+
+import dotenv  from "dotenv"
+dotenv.config()
 
 const app = express();
 
-const port = 9000;
+// app.use("/", (req, res) => {
+//   res.json({ message: "Hello Vercel" });
+// });
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello Vercel" });
 });
 
-app.listen(9000, () => {
-  console.log(`Server is running on Port ${port}`);
+app.get("/login", (req, res) => {
+  res.json({ message: "Login" });
+});
+
+app.get("/register", (req, res) => {
+  res.json({ message: "Register" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on Port ${process.env.PORT}`);
 });
